7ad43117cd0e8eb888502b1e50847813357cd5d7
Thomas Malt <thomas@malt.no>
2017-03-21 11:45:20 +0100

    fixing code to work again, preparing new release

--- test/cron-emitter.test.js
+++ test/cron-emitter.test.js
@@ -10,52 +10,50 @@ const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
 const events = require('events');
-const cron = require('../lib/cron-emitter');
+const timers = require('timers');
+const CronEmitter = require('../lib/cron-emitter');
 
 chai.should();
 
-describe('Create object', function() {
-  it('should return a new CronEmitter object', function() {
-    "use strict";
-    (new cron.CronEmitter()).should.be.instanceOf(cron.CronEmitter);
+describe('Create object', () => {
+  it('should return a new CronEmitter object', () => {
+    (new CronEmitter()).should.be.instanceOf(CronEmitter);
   });
 
-  it('should also be an eventemitter', function() {
-    "use strict";
-    (new cron.CronEmitter()).should.be.instanceOf(events.EventEmitter);
+  it('should also be an eventemitter', () => {
+    (new CronEmitter()).should.be.instanceOf(events.EventEmitter);
   });
 
-  describe('add', function() {
-    "use strict";
-    var cm;
+  describe('add', () => {
+    let cm;
 
-    beforeEach(function() {
-      cm = new cron.CronEmitter();
+    beforeEach(() => {
+      cm = new CronEmitter();
     });
 
-    it('should throw TypeError when called without an argument', function() {
+    it('should throw TypeError when called without an argument', () => {
       cm.add.should.throw(TypeError, /two arguments are mandatory/);
     });
 
-    it('should throw TypeError when called without second argument', function() {
+    it('should throw TypeError when called without second argument', () => {
       cm.add.bind(cm, '*/3 * * * * *').should.throw(TypeError, /two arguments/);
     });
 
-    it('should throw TypeError when called without illegal cron argument', function() {
-      cm.add.bind(cm, 'ninja', 'ninja').should.throw(Error, /first argument/);
+    it('should throw Error when called without illegal cron argument', () => {
+      cm.add.bind(cm, 'ninja', 'ninja').should.throw(Error);
     });
 
-    it('should register an event correct when everyting is correct', function() {
-      expect(cm.add('* * * * * *', "second")).to.be.true;
+    it('should register an event correct when everyting is correct', () => {
+      cm.add('* * * * * *', "second").should.be.a('object');
     });
 
     it('should emit event after one second', function(done) {
-      this.timeout(0);
-      var counter = 0;
-      var errTimeout = setTimeout(function() {
+      this.timeout(4000);
+      let counter = 0;
+      let errTimeout = setTimeout(() => {
         assert.fail("Did not get event");
       }, 3000);
-      cm.on('aSecond', function() {
+      cm.on('aSecond', () => {
         counter++;
         if (counter > 2) {
           clearTimeout(errTimeout);
@@ -77,27 +75,28 @@ describe('Create object', function() {
       var counter = 0;
       var last = null;
       try {
-        while (last = cm._crontab['every_second'].next()) {
+        while (last = cm.crontab['every_second'].next()) {
           counter++;
         }
       } catch (e) {
+        console.log(e.message);
         e.should.be.instanceOf(Error);
-        expect(last).to.be.instanceOf(Date);
-        expect(last).to.not.be.greaterThan(end);
+        expect(last).to.be.a('object');
+        expect(last.getTime()).to.not.be.greaterThan(end.getTime());
         expect(counter).to.equal(3);
         done();
       }
     });
   });
 
-  describe('getEventList', function() {
+  describe('getEventList', () => {
     "use strict";
     var emitter;
-    before(function() {
-      emitter = new cron.CronEmitter();
+    before(() => {
+      emitter = new CronEmitter();
     });
 
-    it('should return a correct list of events', function() {
+    it('should return a correct list of events', () => {
       var list = emitter.getEventList();
       expect(list).to.be.instanceOf(Object);
       expect(Object.keys(list).length).to.equal(0);
@@ -111,52 +110,52 @@ describe('Create object', function() {
 
     });
 
-    after(function() {
+    after(() => {
       emitter = undefined;
     });
   });
 
-  describe('remove', function() {
+  describe('remove', () => {
     "use strict";
     var em;
-    before(function() {
-      em = new cron.CronEmitter();
+    before(() => {
+      em = new CronEmitter();
       em.add('* * * * * *', "every_second");
       em.add('0 * * * *', "every_hour");
     });
 
-    it('should have correct initial content', function() {
+    it('should have correct initial content', () => {
       expect(Object.keys(em.getEventList()).length).to.equal(2);
       expect(Object.keys(em.getEventList())).to.deep.equal([
         "every_second", "every_hour"
       ]);
     });
 
-    it('should delete content correctly', function() {
+    it('should delete content correctly', () => {
       expect(em.remove("every_second")).to.be.true;
       expect(Object.keys(em.getEventList())).to.deep.equal([
         "every_hour"
       ]);
     });
 
-    it('should handle being asked to delete something non existing', function() {
+    it('should handle being asked to delete something non existing', () => {
       expect(em.remove.bind(em, "not_here")).to.throw(TypeError, /No such event/);
     });
   });
 
-  describe('hasEvent', function() {
+  describe('hasEvent', () => {
     "use strict";
     var em;
-    before(function() {
-      em = new cron.CronEmitter();
+    before(() => {
+      em = new CronEmitter();
       em.add('0 0 31 * *', 'everyMonth');
     });
 
-    it('should return true when event exist', function() {
+    it('should return true when event exist', () => {
       em.hasEvent('everyMonth').should.be.true;
     });
 
-    it('should return false when event does not', function() {
+    it('should return false when event does not', () => {
       em.hasEvent('right_now').should.be.false;
     });
 
