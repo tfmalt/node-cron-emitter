44738f65ebb83812f1844318ceb606897eda8758
Thomas Malt <thomas@malt.no>
2017-03-22 15:41:45 +0100

    updating example wip

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
 /**
  * Created by tm on 25/03/15.
  *
- * @author tm
- * @copyright 2015 (c) tm
+ * @author Thomas Malt <thomas@malt.no>
+ * @copyright 2015-2017 (c) Thomas Malt
  */
 
-var CronEmitter = require('./lib/cron-emitter');
+const CronEmitter = require('./lib/cron-emitter');
 
-var emitter = new CronEmitter();
-var now = new Date();
+const emitter = new CronEmitter();
+const now = new Date();
 
 emitter.add('*/3  * * * * *', 'every_three_seconds');
 emitter.add('*/10 * * * * *', 'every_ten_seconds');
@@ -21,34 +21,28 @@ emitter.add('* * * * * *', "every_second_stop", {
 
 console.log("done");
 
-emitter.on('every_three_seconds', function() {
-  "use strict";
+emitter.on('every_three_seconds', () => {
   console.log(Date(), "EVENT: Got every three seconds event.");
 });
 
-emitter.on('every_ten_seconds', function() {
-  "use strict";
+emitter.on('every_ten_seconds', () => {
   console.log(Date(), "EVENT: Got ten seconds event.");
 });
 
 emitter.on('every_minute', function() {
-  "use strict";
   console.log(Date(), "EVENT: A minute has passed");
 });
 
 emitter.on('every_five_minutes', function() {
-  "use strict";
   console.log(Date(), "EVENT: Five minutes has passed");
 });
 
 emitter.on('every_thirty_minutes', function() {
-  "use strict";
   console.log(Date(), "EVENT: Thirty minutes has passed");
 });
 
 var counter = 0;
 emitter.on("every_second_stop", function() {
-  "use strict";
   counter++;
   console.log(Date(), "EVENT: got every second event: ", counter);
 });
