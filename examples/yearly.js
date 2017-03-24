622ec7fd2f1c87ac2b8717561e17269dbb6bd780
Thomas Malt <thomas@malt.no>
2017-03-24 12:21:09 +0100

    fixed bug in dealing with long timeouts (ie. a year). timeouts should now work many years into the future

--- examples/yearly.js
+++ examples/yearly.js
@@ -1,5 +1,5 @@
 /**
- * Example showing the use of remove in cron-emitter.
+ * Example showing the use of yearly as input to cron-emitter.
  *
  * @author Thomas Malt <thomas@malt.no>
  * @copyright 2015-2017 (c) Thomas Malt
@@ -11,4 +11,11 @@ const emitter     = new CronEmitter();
 
 emitter
   .add('@yearly', 'got_it')
+  .add('*/5 * * * * *', 'seconds')
+  .on('removed', (name) => console.log(new Date(), '==> removed', name))
+  .on('seconds', () => console.log(new Date(), '==> 5 seconds'))
   .on('got_it', () => console.log(new Date(), '==> got it:'));
+
+
+setTimeout(() => emitter.remove('got_it'), 15000);
+setTimeout(() => emitter.remove('seconds'), 25000);
